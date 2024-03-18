76434c13e422326ce3e14c147e7174f0d8148494
swjo207 <swjo207@gmail.com>
2024-03-18 16:15:23 +0900

    강의 자료

---
+++ RPG-Tutorial/Firebase/Assets/FastCampus/StatsSystem/Scripts/ModifiableInt.cs
@@ -0,0 +1,82 @@
+﻿using FastCampus.Core;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ModifiableInt
+{
+    [NonSerialized]
+    private int baseValue;
+
+    [SerializeField]
+    private int modifiedValue;
+
+    public int BaseValue
+    {
+        get => baseValue;
+        set
+        {
+            baseValue = value;
+            UpdateModifiedValue();
+        }
+    }
+
+    public int ModifiedValue
+    {
+        get => modifiedValue;
+        set => modifiedValue = value;
+    }
+
+    private event Action<ModifiableInt> OnModifiedValue;
+
+    private List<IModifier> modifiers = new List<IModifier>();
+
+    public ModifiableInt(Action<ModifiableInt> method = null)
+    {
+        ModifiedValue = baseValue;
+        RegisterModEvent(method);
+
+    }
+
+    public void RegisterModEvent(Action<ModifiableInt> method)
+    {
+        if (method != null)
+        {
+            OnModifiedValue += method;
+        }
+    }
+
+    public void UnregisterModEvent(Action<ModifiableInt> method)
+    {
+        if (method != null)
+        {
+            OnModifiedValue -= method;
+        }
+    }
+
+    private void UpdateModifiedValue()
+    {
+        int valueToAdd = 0;
+        foreach (IModifier modifier in modifiers)
+        {
+            modifier.AddValue(ref valueToAdd);
+        }
+
+        ModifiedValue = baseValue + valueToAdd;
+
+        OnModifiedValue?.Invoke(this);
+    }
+
+    public void AddModifier(IModifier modifier)
+    {
+        modifiers.Add(modifier);
+        UpdateModifiedValue();
+    }
+
+    public void RemoveModifier(IModifier modifier)
+    {
+        modifiers.Remove(modifier);
+        UpdateModifiedValue();
+    }
+}
